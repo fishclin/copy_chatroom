e8d2b21d734e0199730134b18d07dc1eb6e831f0
Michael Yu <fish.0217@gmail.com>
2016-08-16 00:04:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,33 +8,53 @@ app.get('/', function(req, res){
 
 var user_count = 0;
 
+
 //當新的使用者連接進來的時候
 io.on('connection', function(socket){
 
-	//新user
+		
+	//監聽新user登入
 	socket.on('add user',function(msg){
-		socket.username = msg;
-		console.log("new user:"+msg+" logged.");
+		
+		user_count++;//增加使用者數量	
+		socket.username = msg.username;
+		
+		console.log("new user:"+msg+" logged. user_count= "+user_count);
 		io.emit('add user',{
-			username: socket.username
+			username: socket.username,
 		});
 	});
 
 	//監聽新訊息事件
 	socket.on('chat message', function(msg){
 
-		console.log(socket.username+":"+msg);
+		console.log(socket.username+":"+msg.username+", userColor:"+msg.userColor);
 
   		//發佈新訊息
 		io.emit('chat message', {
 			username:socket.username,
-			msg:msg
+			msg:msg.text,
+			userColor:msg.userColor
 		});
 	});
 
-	//left
+	//監聽正在打字
+	socket.on('user is typing', function(msg){
+
+		console.log(socket.username+" is typing");
+
+  		//發佈新訊息
+		io.emit('user is typing', {
+			username:socket.username
+		});
+	});
+	
+	////監聽離開聊天室
 	socket.on('disconnect',function(){
-		console.log(socket.username+" left.");
+		user_count--;
+		console.log(socket.username+" left. user_count= "+user_count);
+		
+		//發佈新訊息
 		io.emit('user left',{
 			username:socket.username
 		});
