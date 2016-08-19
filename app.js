e74f4be93207c843ae5d8b907da2ec666f52b90b
Michael Yu <fish.0217@gmail.com>
2016-08-20 02:43:31 +0800

    update onlin-list (add user)

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ app.get('/', function(req, res){
 
 var user_count = 0;
 
+var onlineList_Text = ""; 
+
+// = '{"username": "Guest-Test", "userColor": "yellow" }';
+
+/*var onlineList_Text = 
+'{"username": "jack", "userColor": "blue" },' +
+'{"username": "merry", "userColor": "yellow" },' +
+'{"username": "Michael", "userColor": "orange" },' +
+'{"username": "jane", "userColor": "red"}';
+*/
 
 //當新的使用者連接進來的時候
 io.on('connection', function(socket){
@@ -16,13 +26,40 @@ io.on('connection', function(socket){
 	//監聽新user登入
 	socket.on('add user',function(msg){
 		
-		user_count++;//增加使用者數量	
-		socket.username = msg.username;
+		user_count++;//增加使用者數量
+		socket.username = msg.username;//儲存使用者名稱
 		
-		console.log("new user:"+msg+" logged. user_count= "+user_count);
+		console.log("new user:"+msg.username+" 登入. user_count= "+user_count);
+
+		if(user_count == 1) 
+		{//增加第一個人的 onlineList_Text 格式(前面不用逗號)
+			onlineList_Text = '{"username":"'+ msg.username +'","userColor":"'+ msg.userColor +'"}';
+			console.log("增加第一人 user_count = "+user_count);
+			console.log(onlineList_Text);
+		}
+		else if(user_count > 1)
+		{//增加使用者名單(username & userColor)
+			onlineList_Text = onlineList_Text + ',{"username":"'+ msg.username +'","userColor":"'+ msg.userColor +'"}';
+			console.log("增加一人 user_count= "+user_count);
+		}
+
+		//console.log(onlineList_Text);
+
+		var onlineList = JSON.parse('['+onlineList_Text+']'); //轉成JSON格式
+
+		/*列印 JSON 
+		var i;
+        for(i = 0; i < onlineList.length; i++) 
+        {
+            console.log("onlineList JSON: i="+i+", "+onlineList[i].username+" "+onlineList[i].userColor);
+        }*/
+
+
 		io.emit('add user',{
 			username: socket.username,
+			onlineList: onlineList
 		});
+		
 	});
 
 	//監聽新訊息事件
@@ -49,9 +86,14 @@ io.on('connection', function(socket){
 		});
 	});
 	
-	////監聽離開聊天室
+	//監聽離開聊天室
 	socket.on('disconnect',function(){
-		user_count--;
+		
+		if(user_count > 0)
+		{
+			user_count--; //不允許人數小於0	
+		}
+		
 		console.log(socket.username+" left. user_count= "+user_count);
 		
 		//發佈新訊息
@@ -67,3 +109,11 @@ io.on('connection', function(socket){
 http.listen(process.env.PORT || 3000, function(){
 	console.log('listening on *:3000');
 });
+
+function addUserList(onlineList_Text, username, userColor)
+{
+	onlineList_Text = onlineList_Text + ' ,{"username":" '+ username +' ", "userColor":" '+ userColor +' "} ';
+	return onlineList_Text;
+}
+
+
